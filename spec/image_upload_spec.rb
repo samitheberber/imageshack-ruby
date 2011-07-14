f94f5177c3569435f467301eea232bb9431e4e93
Sami Saada <sami.saada@gmail.com>
2011-07-15 00:45:19 +0300

    added spec structure for image uploading

---
+++ spec/image_upload_spec.rb
@@ -0,0 +1,27 @@
+require "imageshack"
+
+describe ImageShack do
+
+  it "needs API key to be set in environment variable"
+
+  describe "can upload images" do
+
+    describe "anonymously" do
+
+      it "using test image with required parameters"
+
+      it "but fails nicely with wrong parameters"
+
+    end
+
+    describe "for a registered user" do
+
+      it "using username and password from environment variable"
+
+      it "but fails nicely without correct password"
+
+    end
+
+  end
+
+end
